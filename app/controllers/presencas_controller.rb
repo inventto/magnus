ede279280c881a2acc7b4aceb700655a6339db25
Lucas Cardoso de Araujo <lucas.ca.de.araujo@gmail.com>
2013-04-08 08:23:04 -0300

    colocado o logo do magnus e alterado os layout dos scaffold

--- app/controllers/presencas_controller.rb
+++ app/controllers/presencas_controller.rb
@@ -1,5 +1,8 @@
+#coding: utf-8
 class PresencasController < ApplicationController
   active_scaffold :presenca do |conf|
+    conf.label = "Presenças"
+    conf.columns[:presenca].label = "Presença"
     conf.columns = [:aluno, :data, :horario, :presenca, :justificativa_de_falta]
   end
 end
