84ce4b75b5846e0d1f9ca5d576fcf4b100f2b79b
Lucas Cardoso de Araujo <lucas.ca.de.araujo@gmail.com>
2013-04-09 08:08:04 -0300

    arrumado as label que estavam erradas nos feriados e telefones

--- app/controllers/feriados_controller.rb
+++ app/controllers/feriados_controller.rb
@@ -1,10 +1,10 @@
 #coding: utf-8
 class FeriadosController < ApplicationController
   active_scaffold :feriado do |conf|
+    conf.columns = [:descricao, :feriado_fixo, :repeticao_anual, :dia, :mes, :ano]
     conf.columns[:descricao].label = "Descrição"
     conf.columns[:feriado_fixo].label = "Feriado Fixo"
     conf.columns[:repeticao_anual].label = "Repetição Anual"
     conf.columns[:mes].label = "Mês"
-    conf.columns = [:descricao, :feriado_fixo, :repeticao_anual, :dia, :mes, :ano]
   end
 end
--- app/controllers/telefones_controller.rb
+++ app/controllers/telefones_controller.rb
@@ -1,5 +1,9 @@
+#coding: utf-8
 class TelefonesController < ApplicationController
   active_scaffold :telefone do |conf|
     conf.columns = [:ddd, :numero, :descricao, :ramal, :tipo_telefone]
+    conf.columns[:ddd].label = "DDD"
+    conf.columns[:numero].label = "Número"
+    conf.columns[:descricao].label = "Descrição"
   end
 end
