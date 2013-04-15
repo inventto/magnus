0a040d5bec743c6f46df6b6921a9d1e8a2f58efb
Edson Tofolo <edsontofolo@gmail.com>
2013-04-15 08:35:52 -0300

    Arrumado erro que ocorria quando a variavel de instancia nao recebia a variavel local aluno no registrar presenca

--- app/controllers/registro_presenca_controller.rb
+++ app/controllers/registro_presenca_controller.rb
@@ -7,38 +7,37 @@ class RegistroPresencaController < ApplicationController
 
   def registrar
     begin
-      aluno = Aluno.find params[:id]
+      @aluno = Aluno.find params[:id]
     rescue
-      flash[:error] = "Código do Aluno Inválido!"
+      flash[:error] = "Código do aluno Inválido!"
       redirect_to "/registro_presenca"
       return
     end
 
-    if not aluno.registrar_presenca
+    if not @aluno.registrar_presenca
       flash[:error] = "Erro ao Registrar Presença!"
       redirect_to "/registro_presenca"
       return
     end
 
     notice = []
-    if aluno.aula_de_reposicao?
+    if @aluno.aula_de_reposicao?
       flash[:notice] = "Hoje não é seu dia normal de aula!"
       return
     end
-    if aluno.esta_de_aniversario_essa_semana?
+    if @aluno.esta_de_aniversario_essa_semana?
       notice << "Feliz Aniversário!"
     end
-    if aluno.esta_adiantado?
-      notice << "Aguarde... sua aula começa em aproximadamente " << aluno.quantos_min_adiantado << " minutos! "
-    elsif aluno.esta_atrasado?
+    if @aluno.esta_adiantado?
+      notice << "Aguarde... sua aula começa em aproximadamente " << @aluno.quantos_min_adiantado << " minutos! "
+    elsif @aluno.esta_atrasado?
       notice << "Atenção! Você está atrasado! Procure chegar mais cedo na próxima aula!"
     else
       notice << "Bem Vindo à Magnus Personal...Você chegou no horário!"
     end
-    if aluno.primeira_aula?
+    if @aluno.primeira_aula?
       notice << "Bem Vindo à Magnus Personal...Hoje é sua primeira aula!"
     end
     flash[:notice] = notice.join("<br/><br/>").html_safe
-    @aluno = aluno
   end
 end
