cf74ec45a7cd4b29d49acb43dc5b341a02b04c4d
Lucas Cardoso de Araujo <lucas.ca.de.araujo@gmail.com>
2013-04-15 11:56:17 -0300

    feito a validação para data inicial e final arrumado o menu

--- app/models/matricula.rb
+++ app/models/matricula.rb
@@ -7,11 +7,12 @@ class Matricula < ActiveRecord::Base
 
   validates_presence_of :aluno
   validates_presence_of :horario_de_aula
-  validates_numericality_of :numero_de_aulas_previstas
+  validates_presence_of :data_inicio
+  validates_numericality_of :numero_de_aulas_previstas, :unless => "numero_de_aulas_previstas.blank?"
   validate :data_final
 
   def data_final
-    errors.add(:data_fim, "não pode ser menor que Data Inicial!") if data_fim < data_inicio
+    errors.add(:data_fim, "não pode ser menor que Data Inicial!") if data_fim and data_inicio and data_fim < data_inicio
   end
 
   def label
