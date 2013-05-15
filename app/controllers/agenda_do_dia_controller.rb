fcddc33301ed483f83896c738b1cdc5fa7ac15ae
Edson Gustavo Tofolo <edsontofolo@gmail.com>
2013-05-15 10:44:36 -0300

    Incluido o filtro por periodo na agenda do dia, alterado o layout para exibir o dia da semana e a data, o inicio do horario e o fim para cada aluno

--- app/controllers/agenda_do_dia_controller.rb
+++ app/controllers/agenda_do_dia_controller.rb
@@ -7,8 +7,8 @@ class AgendaDoDiaController < ApplicationController
   end
 
   def filtrar
-    @data_inicial = params[:"data-inicial"]
-    @data_final = params[:"data-final"]
+    @data_inicial = Date.parse(params[:"data-inicial"])
+    @data_final = Date.parse(params[:"data-final"])
 
     load_agenda
 
@@ -16,18 +16,16 @@ class AgendaDoDiaController < ApplicationController
   end
 
   def load_agenda
-    agenda_do_dia = HorarioDeAula.joins(:matricula).joins("INNER JOIN alunos ON matriculas.aluno_id=alunos.id")
+    agenda = consulta_agenda
 
-    if @data_inicial == @data_final
-      dia_da_semana = Date.parse(@data_inicial).wday rescue @data_inicial.wday
-      agenda_do_dia = agenda_do_dia.where(:"horarios_de_aula.dia_da_semana" => dia_da_semana)
-    end
+    reposicao_do_dia = consulta_reposicao
 
-    #agenda_do_dia = agenda_do_dia.where("data_inicio <= '#{@data_inicial}' and (data_fim >= '#{@data_final}' or data_fim is null)")
-    agenda_do_dia = agenda_do_dia.where("data_fim >= '#{@data_final}' or data_fim is null")
+    concatena_horarios(agenda, reposicao_do_dia)
 
-#    agenda_do_dia = agenda_do_dia.where(:"horarios_de_aula.dia_da_semana" => Time.now.wday).where("data_inicio <= current_date and (data_fim >= current_date or data_fim is null)")
+    agrupa_e_ordena
+  end
 
+  def consulta_reposicao
     reposicao_do_dia = Presenca.select("presencas.*, EXTRACT( DOW FROM data ) AS dia_da_semana").joins(:aluno)
 
     if @data_inicial == @data_final
@@ -37,38 +35,36 @@ class AgendaDoDiaController < ApplicationController
     end
 
     reposicao_do_dia = reposicao_do_dia.where(:reposicao => true)
+  end
 
-#    reposicao_do_dia = Presenca.joins(:aluno).where(:data => Date.today).where(:reposicao => true)
+  def consulta_agenda
+    agenda = HorarioDeAula.joins(:matricula).joins("INNER JOIN alunos ON matriculas.aluno_id=alunos.id")
 
-    @agenda_do_dia = []
-
-    agenda_do_dia.each do |a|
-      @agenda_do_dia << a
+    if @data_inicial == @data_final
+      dia_da_semana = @data_inicial.wday
+      agenda = agenda.where(:"horarios_de_aula.dia_da_semana" => dia_da_semana)
     end
 
-    reposicao_do_dia.each do |r|
-      @agenda_do_dia << r
-    end
+    agenda
+  end
 
+  def agrupa_e_ordena
     @agenda_do_dia = @agenda_do_dia.group_by{ |a| a.dia_da_semana.to_i }
 
     @agenda_do_dia.each do |k, agenda|
-      agenda.sort! {|x,y| (x.horario[0..1].to_i * 3600 + x.horario[1..2].to_i * 60) <=> (y.horario[0..1].to_i * 3600 + y.horario[1..2].to_i * 60)}
+      agenda.sort! {|x, y| (x.horario[0..1].to_i * 3600 + x.horario[1..2].to_i * 60) <=> (y.horario[0..1].to_i * 3600 + y.horario[1..2].to_i * 60)}
     end
-
-=begin    temp = []
-    agenda = []
-    @agenda_do_dia.each do |a|
-      if not temp.blank? and temp.last.dia_da_semana.to_i != a.dia_da_semana.to_i
-        agenda << temp
-        temp = []
-      end
-      temp << a
-    end
-    agenda << temp
-    @agenda_do_dia = agenda
-    @agenda_do_dia.sort! {|x,y| (x.horario[0..1].to_i * 3600 + x.horario[1..2].to_i * 60) <=> (y.horario[0..1].to_i * 3600 + y.horario[1..2].to_i * 60)}
-=end
   end
 
+  def concatena_horarios agenda, reposicao
+    @agenda_do_dia = []
+
+    agenda.each do |a|
+      @agenda_do_dia << a
+    end
+
+    reposicao.each do |r|
+      @agenda_do_dia << r
+    end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,14 @@
 #coding: utf-8
 module ApplicationHelper
-  def status_presenca aluno_id, presenca_id
+  def status_presenca agenda
     presenca = Presenca.joins("LEFT JOIN justificativas_de_falta ON presencas.id=presenca_id")
+    begin
+      aluno_id = agenda.matricula.aluno.id
+      presenca_id = ""
+    rescue
+      aluno_id = agenda.aluno.id
+      presenca_id = agenda.id
+    end
     if presenca_id.blank?
       presenca = presenca.where(:aluno_id => aluno_id, :data => Time.now).where("reposicao is null or reposicao = false")
     else
@@ -31,5 +38,13 @@ module ApplicationHelper
         return retorno
       end
     end
+  end
+
+  def final_do_horario horario
+    (Time.strptime(horario,"%H:%M") + 3600).strftime("%H:%M")
+  end
+
+  def exibir_aluno? aluno_id, dia_atual
+     Matricula.where("data_inicio <= '#{dia_atual}' and (data_fim >= '#{dia_atual}' or data_fim is null)").find_by_aluno_id(aluno_id)
   end
 end
