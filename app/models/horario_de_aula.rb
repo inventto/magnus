d6b7167b4eb4dfb7b4c948696d2351099238dfc9
Edson Gustavo Tofolo <edsontofolo@gmail.com>
2013-05-21 14:36:36 -0300

    Removido as constantes MONTHNAMES e DAYNAMES do model HorarioDeAula

--- app/models/horario_de_aula.rb
+++ app/models/horario_de_aula.rb
@@ -6,8 +6,6 @@ class HorarioDeAula < ActiveRecord::Base
   validates_presence_of :horario
 
   DIAS = {:"Domingo"=> "0", :"Segunda" => "1", :"Terça" => "2", :"Quarta" => "3", :"Quinta" => "4", :"Sexta" => "5", :"Sábado" => "6"}
-  DAYNAMES = %w{domingo segunda-feira terça-feira quarta-feira quinta-feira sexta-feira sábado}
-  MONTHNAMES = %w{Janeiro Fevereiro Março Abril Maio Junho Julho Agosto Setembro Outubro Novembro Dezembro}
 
   def label
     desc = ""
