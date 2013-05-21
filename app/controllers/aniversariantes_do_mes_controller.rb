68405ed8d6e1371a7ccb1eee378d1ec047ac1793
Edson Gustavo Tofolo <edsontofolo@gmail.com>
2013-05-21 15:59:33 -0300

    Criada a tabela de aniversariantes do mes

--- app/controllers/aniversariantes_do_mes_controller.rb
+++ app/controllers/aniversariantes_do_mes_controller.rb
@@ -1,5 +1,7 @@
 class AniversariantesDoMesController < ApplicationController
   def aniversariantes
-    @aniversariantes = Aluno.where("extract(month From data_nascimento) = extract(month from now())").group(:data_nascimento, :id)
+    @aniversariantes = Aluno.where("extract(month from data_nascimento) = extract(month from now())").group(:data_nascimento, :id)
+    @aniversariantes_de_hoje = @aniversariantes.where("extract(day from data_nascimento) = extract(day from current_date)")
+    @aniversariantes_do_mes = @aniversariantes.where("extract(day from data_nascimento) <> extract(day from current_date)")
   end
 end
