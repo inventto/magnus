f552feb5e682ef4b86311cef2a053c7c81483fc7
Edson Tofolo <edsontofolo@gmail.com>
2013-06-04 11:08:07 -0300

    Criado o quadro de horarios

--- app/controllers/quadro_de_horarios_controller.rb
+++ app/controllers/quadro_de_horarios_controller.rb
@@ -1,5 +1,8 @@
 class QuadroDeHorariosController < ApplicationController
   def index
     @horarios = HorarioDeAula.joins(:matricula).order(:dia_da_semana).where("matriculas.data_inicio <= current_date and (matriculas.data_fim >= current_date or matriculas.data_fim is null)").group_by{|h| h.dia_da_semana}
+    @horarios.each do |k, v|
+      @horarios[k] = v.group_by{|h| h.horario}
+    end
   end
 end
