6dc02284eb8a97d55f331b9da9d1f821f080a2eb
Edson Tofolo <edsontofolo@gmail.com>
2013-06-10 09:14:33 -0300

    Caso nao tenha sido informada a data de matricula ao fazer a consulta do quadro de horarios eh considerada a data de inicio

--- app/controllers/quadro_de_horarios_controller.rb
+++ app/controllers/quadro_de_horarios_controller.rb
@@ -1,6 +1,6 @@
 class QuadroDeHorariosController < ApplicationController
   def index
-    @horarios = HorarioDeAula.joins(:matricula).order(:dia_da_semana).where("matriculas.data_inicio <= current_date and (matriculas.data_fim >= current_date or matriculas.data_fim is null)").group_by{|h| h.dia_da_semana}
+    @horarios = HorarioDeAula.joins(:matricula).order(:dia_da_semana).where("coalesce(matriculas.data_matricula,matriculas.data_inicio) <= current_date and (matriculas.data_fim >= current_date or matriculas.data_fim is null)").group_by{|h| h.dia_da_semana}
     @horarios.each do |k, v|
       @horarios[k] = v.group_by{|h| h.horario}
     end
