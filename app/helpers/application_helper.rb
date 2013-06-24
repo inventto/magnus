c6a96b6321fb5c5c7b039b94be2c305b7df8ce7c
Edson Tofolo <edsontofolo@gmail.com>
2013-06-24 10:06:25 -0300

    Criado os titulos dos icones na agenda do dia, quando for reposição ou adiantamento. @edsontofolo 1,5h

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,8 @@
 #coding: utf-8
 module ApplicationHelper
   def status_presenca agenda, dia_atual
+    @hora_certa = (Time.now + Time.zone.utc_offset)
+
     if agenda.instance_of? HorarioDeAula
       aluno_id = agenda.matricula.aluno.id
       presenca_id = ""
@@ -37,16 +39,16 @@ module ApplicationHelper
         if presenca.justificativa_de_falta.nil?
           retorno = "<img src='/assets/falta_sem_justif.png' title='Falta Sem Justificativa' />"
         else
-          retorno = "<img src='/assets/falta_justif.png' title='Falta Justificada' />"
+          retorno = "<img src='/assets/falta_justif.png' title='#{get_title_realocacao(aluno_id, dia_atual, presenca)}' />"
         end
         if presenca.realocacao
           hora_atual = get_in_seconds()
           hora_presenca = get_in_seconds(presenca.horario)
 
-          if (presenca.data == (Time.now + Time.zone.utc_offset).to_date) and ( (hora_atual > hora_presenca) and (hora_atual < (hora_presenca + 3600)) )
-            retorno = "<img class='realocacao' src='/assets/realocacao.png' title='Reposição' />"
+          if (presenca.data == @hora_certa.to_date) and ( (hora_atual > hora_presenca) and (hora_atual < (hora_presenca + 3600)) )
+            retorno = "<img class='realocacao' src='/assets/realocacao.png' title='#{get_title_realocacao(aluno_id, dia_atual, presenca)}' />"
           else
-            retorno << "<img class='realocacao' src='/assets/realocacao.png' title='Reposição' />"
+            retorno << "<img class='realocacao' src='/assets/realocacao.png' title='#{get_title_realocacao(aluno_id, dia_atual, presenca)}' />"
           end
         end
         retorno = (aniversario << retorno)
@@ -62,17 +64,41 @@ module ApplicationHelper
     if presenca.presenca? and presenca.realocacao? and not presenca.data_de_realocacao.blank? and not presenca.tem_direito_a_reposicao?
       p = Presenca.joins(:justificativa_de_falta).where(:aluno_id => aluno_id, :data => presenca.data_de_realocacao).where("justificativas_de_falta.descricao ilike '%adiantado%'")
       if not p.blank?
-        title = "Adiantamento do dia #{presenca.data_de_realocacao.strftime("%d/%m/%Y")}, horário #{p[0].horario}"
+        title = "Adiantamento do dia #{presenca.data_de_realocacao.strftime("%d/%m/%Y")}, horário das #{p[0].horario}"
+      else
+        title = "Realocação"
       end
+    elsif not presenca.presenca? and presenca.data_de_realocacao.blank? and not presenca.justificativa_de_falta.nil?
+        p = Presenca.order("id DESC").find_by_data_de_realocacao_and_aluno_id(presenca.data, aluno_id) #em ordem descrescente pois caso haja mais de uma realocação para esse dia
+        if not p.nil?
+          m = presenca.justificativa_de_falta.descricao.match(/\d{1,2}:\d{1,2}/)
+          (not m.nil?) ? horario = m[0] : ""
+
+          if presenca.data == p.data # ainda pode ser reposição ou adiantamento, depende do horario
+            if get_in_seconds(presenca.horario) > get_in_seconds(p.horario) # adiantamento
+              title = "Falta Justificada com Adiantamento para o dia #{presenca.data.strftime("%d/%m/%Y")} às #{horario}"
+            else # reposicao
+              title = "Falta Justificada com Reposição Agendada para o dia #{p.data.strftime("%d/%m/%Y")} às #{p.horario}"
+            end
+          elsif presenca.data > p.data # adiantamento
+              title = "Falta Justificada com Adiantado para o dia #{presenca.data.strftime("%d/%m/%Y")} às #{horario}"
+          else # reposicao
+              title = "Falta Justificada com Reposição Agendada para o dia #{p.data.strftime("%d/%m/%Y")} às #{p.horario}"
+          end
+        else # se houve a falta justificada mas ainda não foi criada a reposição
+          title = "Falta Justificada"
+        end
+    else
+        title = "Reposição Referente à Falta do dia #{presenca.data_de_realocacao.strftime("%d/%m/%Y")}"
     end
-      #p = Presenca.joins(:justificativa_de_falta).where(:aluno_id => aluno_id, :data => dia_atual).where("justificativas_de_falta.descricao ilike '%adiantado%'")
+    title
   end
 
   def get_in_seconds(hour = "")
     if not hour.blank?
       return Time.strptime(hour, "%H:%M").seconds_since_midnight
     else
-      return Time.now.seconds_since_midnight
+      return @hora_certa.seconds_since_midnight #Time.now.seconds_since_midnight
     end
   end
 
