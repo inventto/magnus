f8d04deaf85f0a7983f091881e24f2e7c911cebb
Edson Tofolo <edsontofolo@gmail.com>
2013-07-02 15:08:02 -0300

    Deixado o if tradicional para não ultrapassar limites. refs #7

--- app/models/presenca.rb
+++ app/models/presenca.rb
@@ -13,7 +13,9 @@ class Presenca < ActiveRecord::Base
 
   validates_each :data_de_realocacao do |model, attr, value|
     if not value.blank?
-      model.errors.add(attr, ": Aluno não possui horário de aula na(o) #{Date::DAYNAMES[value.wday].humanize}") if HorarioDeAula.do_aluno_pelo_dia_da_semana(model.aluno_id, value.wday).blank?
+      if HorarioDeAula.do_aluno_pelo_dia_da_semana(model.aluno_id, value.wday).blank?
+        model.errors.add(attr, ": Aluno não possui horário de aula na(o) #{Date::DAYNAMES[value.wday].humanize}")
+      end
     end
   end
 
