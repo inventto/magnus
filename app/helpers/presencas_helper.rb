59acabfd44312317b74180543a4a18a73f0daa66
Edson Tofolo <edsontofolo@gmail.com>
2013-07-03 08:43:06 -0300

    Criada a opção de busca por justificativa de falta no cadastro de presenças. @edsontofolo 0.5h it closes #8

--- app/helpers/presencas_helper.rb
+++ app/helpers/presencas_helper.rb
@@ -1,2 +1,12 @@
+#coding: utf-8
 module PresencasHelper
+  def justificativa_de_falta_search_column(record, html_options)
+    selected = html_options.delete :value
+
+    select_options = ["Não Possui", "Possui"]
+
+    options = { :selected => selected,
+                :include_blank => as_(:_select_)}
+    select(:record, :justificativa_de_falta, select_options, options, html_options)
+  end
 end
