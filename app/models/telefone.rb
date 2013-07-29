fccee87775b6e3eadcc0ba582ee413fe96c4583c
Edson Tofolo <edsontofolo@gmail.com>
2013-07-29 11:24:26 -0300

    Criada as validações para o numero do telefone. @edsontofolo 1h it closes #35

--- app/models/telefone.rb
+++ app/models/telefone.rb
@@ -1,19 +1,34 @@
+#coding: utf-8
 class Telefone < ActiveRecord::Base
   attr_accessible :aluno_id, :ddd, :descricao, :numero, :ramal, :tipo_telefone_id, :tipo_telefone
 
   belongs_to :tipo_telefone
   belongs_to :aluno
 
+  validates_presence_of :ddd, :unless => "numero.blank?"
+  validates_presence_of :numero, :unless => "ddd.blank?"
+  validates_presence_of :tipo_telefone, :if => "not ddd.blank? and not numero.blank?"
+  validates_each :numero do |model, attr, value|
+    if not value.blank?
+      number = value.gsub(/[\s-]/,"")
+      if not (8..9).cover?(number.length)
+        model.errors.add(attr, "Inválido")
+      end
+    end
+  end
+
   def label
     desc = ""
-    desc = "(" << self.ddd.gsub(/\D/,"") << ") "
-    numero = self.numero.gsub(/\D/,"")
-    if numero.length == 8
-      desc << numero[0..3] << " -  " << numero[4..8]
-    elsif numero.length == 9
-      desc << numero[0..4] << " -  " << numero[5..9]
-    else
-      desc << numero
+    if not self.ddd.nil? and not self.numero.nil?
+      desc = "(" << self.ddd.gsub(/\D/,"") << ") "
+      numero = self.numero.gsub(/\D/,"")
+      if numero.length == 8
+        desc << numero[0..3] << " -  " << numero[4..8]
+      elsif numero.length == 9
+        desc << numero[0..4] << " -  " << numero[5..9]
+      else
+        desc << numero
+      end
     end
     desc
   end
