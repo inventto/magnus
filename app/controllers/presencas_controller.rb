5f83aa26b7418e7c41861f1df2da1266a2d60d25
Edson Tofolo <edsontofolo@gmail.com>
2013-08-02 11:23:43 -0300

    Criado combo na busca para usuário informar a quantidade de registros que deseja exibir por consulta. @edsontofolo 3h it closes #41

--- app/controllers/presencas_controller.rb
+++ app/controllers/presencas_controller.rb
@@ -1,8 +1,10 @@
 #coding: utf-8
 class PresencasController < ApplicationController
+  before_filter :update_config
+
   active_scaffold :presenca do |conf|
-    #list.per_page = 64
     conf.label = "Presenças"
+    conf.columns << :quantidade_de_registros
     conf.columns[:presenca].label = "Presença"
     conf.columns[:horario].label = "Horário"
     conf.columns[:realocacao].label = "Realocação de Horário"
@@ -11,11 +13,19 @@ class PresencasController < ApplicationController
     conf.columns[:data_de_realocacao].label = "Realocado de"
     conf.columns = [:aluno, :data, :horario, :pontualidade, :presenca, :realocacao, :data_de_realocacao, :tem_direito_a_reposicao, :aula_extra, :justificativa_de_falta]
     conf.columns[:aluno].form_ui = :select
+    conf.columns[:quantidade_de_registros].form_ui = :select
+    conf.columns[:quantidade_de_registros].options = {:options => Presenca::QUANTIDADES_DE_REGISTROS.map(&:to_sym)}
     conf.columns[:justificativa_de_falta].allow_add_existing = false
     conf.actions.swap :search, :field_search
     conf.field_search.human_conditions = true
-    conf.field_search.columns = [:aluno, :data, :horario, :pontualidade, :presenca, :realocacao, :data_de_realocacao, :tem_direito_a_reposicao, :aula_extra, :justificativa_de_falta]
+    conf.field_search.columns = [:aluno, :data, :horario, :pontualidade, :presenca, :realocacao, :data_de_realocacao, :tem_direito_a_reposicao, :aula_extra, :justificativa_de_falta, :quantidade_de_registros]
     list.sorting = [{:data => 'DESC'}, {:horario => 'DESC'}]
+  end
+
+  def update_config
+    existe_qtd_de_registros = (params and params["search"] and not params["search"]["quantidade_de_registros"].blank?)
+    quantidade_de_registros = (existe_qtd_de_registros) ? params["search"]["quantidade_de_registros"].to_i : 15
+    active_scaffold_config.list.per_page = quantidade_de_registros
   end
 
   def self.condition_for_justificativa_de_falta_column(column, value, like_pattern)
@@ -31,16 +41,16 @@ class PresencasController < ApplicationController
   end
 
   def redirect_page_to_index
-    url = (is_last_page_agenda_do_dia?) ? agenda_do_dia_path : presencas_path
+    url = (the_last_page_is_the_agenda_do_dia?) ? agenda_do_dia_path : presencas_path
     url
   end
 
   def verifica_ultima_pagina_acessada
-    session[:latest_pages_visited].delete_at(session[:latest_pages_visited].length - 1) if is_last_page_agenda_do_dia?
+    session[:latest_pages_visited].delete_at(session[:latest_pages_visited].length - 1) if the_last_page_is_the_agenda_do_dia?
     render :nothing => true
   end
 
-  def is_last_page_agenda_do_dia?
+  def the_last_page_is_the_agenda_do_dia?
     length = session[:latest_pages_visited].length
     last_page_visited = session[:latest_pages_visited][length - 2]
     last_page_visited[:controller] == "agenda_do_dia"
--- app/models/presenca.rb
+++ app/models/presenca.rb
@@ -19,6 +19,11 @@ class Presenca < ActiveRecord::Base
     end
   end
 
+  QUANTIDADES_DE_REGISTROS = %w(10 20 30 40 50 60 70 80 90 100)
+
+  def quantidade_de_registros
+  end
+
   def label
     "presença de " << aluno.nome
   end
