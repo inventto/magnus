4715dab99a809f159dadd01a827cc39c369c0239
Edson Tofolo <edsontofolo@gmail.com>
2013-08-09 09:40:29 -0300

    Arrumado para apenas filtrar por funcionario quando estiver na pagina Registro de ponto. @edsontofolo 0.17

--- app/helpers/registros_de_ponto_helper.rb
+++ app/helpers/registros_de_ponto_helper.rb
@@ -1,6 +1,6 @@
 module RegistrosDePontoHelper
   def options_for_association_conditions(association)
-    if association.name == :pessoa
+    if association.active_record == RegistroDePonto and association.name == :pessoa
       {'pessoas.e_funcionario' => true}
     else
       super
