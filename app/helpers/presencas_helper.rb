142d4b3d994a25af7d666bb0a974c122cf21ee6a
Edson Tofolo <edsontofolo@gmail.com>
2013-08-12 16:01:17 -0300

    Exibido os telefones dos alunos na pagina de Presenças. @edsontofolo 1.5h refs #50

--- app/helpers/presencas_helper.rb
+++ app/helpers/presencas_helper.rb
@@ -1,5 +1,17 @@
 #coding: utf-8
 module PresencasHelper
+  def pessoa_column(record, column)
+    nome = record.pessoa.nome << "<br/>"
+    fones = []
+    if not record.pessoa.telefones.nil?
+      record.pessoa.telefones.each do |telefone|
+        fones << telefone.label
+      end
+    end
+    fones = "<div class='fones'>" << fones.join("<br/>") << "</div>"
+    raw(nome + fones)
+  end
+
   def justificativa_de_falta_search_column(record, html_options)
     selected = html_options.delete :value
 
