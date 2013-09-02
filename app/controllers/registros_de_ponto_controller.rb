b1e5da6a1344f80819fa04683bbee8bbe62c4dcc
Edson Tofolo <edsontofolo@gmail.com>
2013-09-02 16:02:14 -0300

    Inserido campo admin na tabela de users para identificar qual usuario tem permissões de admin. pair @lucascardoso 2.5h it closes #59

--- app/controllers/registros_de_ponto_controller.rb
+++ app/controllers/registros_de_ponto_controller.rb
@@ -1,5 +1,15 @@
 #coding: utf-8
 class RegistrosDePontoController < ApplicationController
+  begin before_filter :is_admin
+    def is_admin
+      if current_user.admin
+        return
+      end
+      flash[:error] = "Você não possui permissão para acessar essa página!"
+      redirect_to :root
+    end
+  end
+
   active_scaffold :registro_de_ponto do |conf|
     conf.label = "Registros de Ponto"
     conf.columns[:hora_de_saida].label = "Hora de saída"
@@ -10,4 +20,5 @@ class RegistrosDePontoController < ApplicationController
     conf.field_search.columns = [:pessoa, :data, :hora_de_chegada, :hora_de_saida]
     list.sorting = [{:data => 'DESC'}, {:hora_de_chegada => 'DESC'}]
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130806185845) do
+ActiveRecord::Schema.define(:version => 20130902180547) do
 
   create_table "bairros", :force => true do |t|
     t.string   "nome"
@@ -143,8 +143,8 @@ ActiveRecord::Schema.define(:version => 20130806185845) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -153,8 +153,9 @@ ActiveRecord::Schema.define(:version => 20130806185845) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
+    t.boolean  "admin",                  :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
