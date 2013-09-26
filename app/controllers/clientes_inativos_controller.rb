111fec69de2393bb54546ad2c59ca7966134875b
Edson Tofolo <edsontofolo@gmail.com>
2013-09-26 12:00:12 -0300

    Melhorado o layout, incluindo validacoes e uma img. @edsontofolo 3h refs #61

--- app/controllers/clientes_inativos_controller.rb
+++ app/controllers/clientes_inativos_controller.rb
@@ -14,7 +14,29 @@ class ClientesInativosController < ApplicationController
     if dias_da_semana = params["dias_da_semana"] and not dias_da_semana.blank?
       dias = []
       dias = dias_da_semana.split(",")
-      @matriculas = @matriculas.where("matriculas.id IN (SELECT matricula_id FROM horarios_de_aula WHERE dia_da_semana IN (?) GROUP BY matricula_id)", dias)
+      mats = Matricula.select("count(dia_da_semana), matricula_id").joins(:horario_de_aula).group(:matricula_id).having("count(dia_da_semana) = ?", dias.length)
+      if not mats.blank?
+        matriculas_id = []
+        mats.each {|m| matriculas_id << m.matricula_id }
+      end
+      mats = Matricula.where("id IN (?)", matriculas_id)
+      matriculas_id = []
+      mats.each do |matricula|
+        i = 0
+        found = true
+        matricula.horario_de_aula.order(:dia_da_semana).each do |horario|
+          if horario.dia_da_semana != dias[i]
+            found = false
+            break
+          end
+          i += 1
+        end
+        if found
+          matriculas_id << matricula.id
+        end
+      end
+      #@matriculas = @matriculas.where("matriculas.id IN (SELECT matricula_id FROM horarios_de_aula WHERE dia_da_semana IN (?) GROUP BY matricula_id)", dias)
+      @matriculas = @matriculas.where("id IN (?)", matriculas_id)
       all_blank = false
     elsif freq_semanal = params["freq_semanal"] and not freq_semanal.blank?
       mats = Matricula.select("count(dia_da_semana), matricula_id").joins(:horario_de_aula).group(:matricula_id).having("count(dia_da_semana) = ?", freq_semanal.to_i)
@@ -22,7 +44,7 @@ class ClientesInativosController < ApplicationController
         matriculas_id = []
         mats.each {|m| matriculas_id << m.matricula_id }
       end
-      @matriculas = @matriculas.where("id IN (?)", matriculas_ids)
+      @matriculas = @matriculas.where("id IN (?)", matriculas_id)
       all_blank = false
     end
     if mes_da_interrupcao = params["mes_da_interrupcao"] and not mes_da_interrupcao.blank?
