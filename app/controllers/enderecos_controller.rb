ad2ebae907936026ae44061592bdeafb7eb96353
Edson Tofolo <edsontofolo@gmail.com>
2013-10-15 09:31:41 -0300

    Arrumado ordem dos campos do endereço. @edsontofolo 0.17h refs #63

--- app/controllers/enderecos_controller.rb
+++ app/controllers/enderecos_controller.rb
@@ -1,7 +1,5 @@
 #coding: utf-8
 class EnderecosController < ApplicationController
   active_scaffold :endereco do |conf|
-    conf.columns = [:cep, :cidade, :logradouro, :numero, :complemento, :bairro ]
-    conf.columns[:numero].label = "Número"
   end
 end
