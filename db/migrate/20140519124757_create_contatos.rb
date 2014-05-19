7bdb32ffd7ecf6a9c185dea5db1d0111ab7b7028
Lucas Cardoso de Araujo <lucas.ca.de.araujo@gmail.com>
2014-05-19 13:35:40 -0300

    Adicionado a tabela contato.

---
+++ db/migrate/20140519124757_create_contatos.rb
@@ -0,0 +1,12 @@
+class CreateContatos < ActiveRecord::Migration
+  def change
+    create_table :contatos do |t|
+      t.text :descricao
+      t.date :data_contato
+      t.references :pessoa
+
+      t.timestamps
+    end
+    add_index :contatos, :pessoa_id
+  end
+end
