6b82266051ac589f4ac2769c59c3939d4e999ae9
Lucas Cardoso de Araujo <lucas.ca.de.araujo@gmail.com>
2014-07-04 14:23:03 -0300

    Limitando o número de alunos por hora.

--- app/helpers/interesse_no_horario_helper.rb
+++ app/helpers/interesse_no_horario_helper.rb
@@ -1,7 +1,7 @@
 #encoding: utf-8
 module InteresseNoHorarioHelper
   def get_nomes_nos(horarios)
-    horarios.collect do |registro|
+    horarios[0,4].collect do |registro|
       ultimo_nome = []
       ultimo_nome = registro.matricula.pessoa.segundo_nome.split(" ")
       ultimo_nome = ultimo_nome[ultimo_nome.length - 1][0]
