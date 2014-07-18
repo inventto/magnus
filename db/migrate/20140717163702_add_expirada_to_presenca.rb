3dabee14f9030e7a07f12e1177ac3d1dfdb7ebc2
Lucas Cardoso de Araujo <lucas.ca.de.araujo@gmail.com>
2014-07-18 11:25:01 -0300

    modificado a migration da falta expirada.

--- db/migrate/20140717163702_add_expirada_to_presenca.rb
+++ db/migrate/20140717163702_add_expirada_to_presenca.rb
@@ -1,6 +1,10 @@
 class AddExpiradaToPresenca < ActiveRecord::Migration
   def self.up
     add_column :presencas, :expirada, :boolean, default: false
+    Pessoa.all.each do |p|
+      p.set_faltas_expiradas(p.id)
+      p.save!
+    end
   end
 
   def self.down
