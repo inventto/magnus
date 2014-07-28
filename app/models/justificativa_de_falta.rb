0a8070103020b38ebe08b12a838d6c3025f0a1ab
Lucas Cardoso de Araujo <lucas.ca.de.araujo@gmail.com>
2014-07-28 10:57:39 -0300

    Removido as validações da justificativa de falta.

--- app/models/justificativa_de_falta.rb
+++ app/models/justificativa_de_falta.rb
@@ -2,10 +2,6 @@
 class JustificativaDeFalta < ActiveRecord::Base
   attr_accessible :descricao, :data, :hora
 
-  validates_presence_of :descricao
-  regex_hora =/(^\d{2})+([:])(\d{2}$)/
-  validates_format_of :hora, with: regex_hora, :message => 'inválida!'
-
   belongs_to :presenca
 
   def label
