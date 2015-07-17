1e09c6364114b7b34c686249224fa688a9703815
Lucas Cardoso de Araujo <lucas.ca.de.araujo@gmail.com>
2015-07-17 16:49:49 -0300

    Adicionado o ano para o relatório de alunos ativos e inativos.

--- app/controllers/lista_de_alunos_ativos_inativos_controller.rb
+++ app/controllers/lista_de_alunos_ativos_inativos_controller.rb
@@ -4,14 +4,40 @@ class ListaDeAlunosAtivosInativosController < ApplicationController
 
   def ativos_inativos
     conditions = [[]]
+    ano = params[:anos]
     if params[:ativos_matriculados] == "on"
-      conditions[0] << "exists (select 1 from matriculas where (inativo_desde is null or not (now() between inativo_desde and coalesce(inativo_ate, now()))) and pessoa_id = pessoas.id and data_fim is null)"
+      if !ano.blank?
+        conditions[0] << "exists (select 1 from matriculas where 
+          (inativo_desde is null or not (extract (year from inativo_desde)) ="+ano+" and 
+          (extract (year from inativo_desde)) ="+ano+") and 
+          pessoa_id = pessoas.id and data_fim is null)"
+      else
+      conditions[0] << "exists (select 1 from matriculas 
+        where (inativo_desde is null or not 
+        (now() between inativo_desde and 
+        coalesce(inativo_ate, now()))) and 
+        pessoa_id = pessoas.id and data_fim is null)"
+      end
     end
     if params[:inativos] == "on"
-      conditions[0] << "exists (select 1 from matriculas where (now() between inativo_desde and coalesce(inativo_ate, now()) and data_fim is null) and pessoa_id = pessoas.id)"
+      if !ano.blank?
+        conditions[0] << "exists (select 1 from matriculas where 
+          ((extract (year from inativo_desde)) ="+ano+" and 
+          (extract (year from inativo_desde)) ="+ano+") and 
+          pessoa_id = pessoas.id and data_fim is null)"
+      else
+        conditions[0] << "exists (select 1 from matriculas
+          where (now() between inativo_desde and 
+          coalesce(inativo_ate, now()) and data_fim is null) 
+          and pessoa_id = pessoas.id)"
+      end
     end
     if params[:interrompidos] == "on"
-      conditions[0] << "not exists (select 1 from matriculas where data_fim is null and pessoa_id = pessoas.id)"
+      if !ano.blank?
+        conditions[0] << "exists (select 1 from matriculas where (extract (year from data_fim) = "+ano+") and pessoa_id = pessoas.id)"
+      else
+        conditions[0] << "not exists (select 1 from matriculas where data_fim is null and pessoa_id = pessoas.id)"
+      end
     end
 
     conditions[0] = conditions[0].join(" or ")
