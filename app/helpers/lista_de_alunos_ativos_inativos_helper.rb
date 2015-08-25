0d7b31984c49b128b1ab463b1f2b0c69b5c13ccf
Lucas Cardoso de Araujo <lucas.ca.de.araujo@gmail.com>
2015-08-25 14:19:46 -0300

    Adicionado um novo filtro no relatório de alunos ativos e não ativos.

--- app/helpers/lista_de_alunos_ativos_inativos_helper.rb
+++ app/helpers/lista_de_alunos_ativos_inativos_helper.rb
@@ -1,3 +1,10 @@
 # -*- encoding : utf-8 -*-
 module ListaDeAlunosAtivosInativosHelper
+  def com_objetivo_primario(pessoa)
+    if pessoa.matriculas
+      if pessoa.matriculas.valida.first
+        pessoa.matriculas.valida.first.objetivo_primario
+      end
+    end
+  end
 end
